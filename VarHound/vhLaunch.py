7fb0e1f1ff220e1dfcde3f7452c942eb1e017ad3
Luciano Giaco <964424-lgiaco@users.noreply.gitlab.com>
2021-12-27 07:31:52 +0000

    Upload New File

---
+++ VarHound/vhLaunch.py
@@ -0,0 +1,38 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# VarHound - TSO500 - Coverage diagnostics launcher
+
+import os
+import sys
+from subprocess import call
+
+# VarHound absolute path
+VarHound = "~/VarHound"
+
+# Coverage (unzipped) file extension
+suffix = "thresholds.bed"
+
+
+covpath = os.path.normpath(sys.argv[1])
+vhrunx = ' ' + os.path.join(VarHound, "vhLaunch.R")
+vhcore = ' ' + os.path.join(VarHound, "varhound.R")
+vhcovr = ' ' + os.path.join(VarHound, "vhCoverage.R")
+covdir = ' ' + covpath
+
+call('gzip -rdkf' + covdir, shell = True)
+
+f = []
+for root, sub, files in os.walk(covpath):
+	for x in files:
+		if x.endswith(suffix):
+			f += [os.path.join(root, x)]
+
+f = ' ' + str(len(f))
+
+if len(sys.argv) > 2:
+	run = ' ' + sys.argv[2]
+else:
+	run = ' snv'
+
+call('Rscript' + vhrunx + vhcore + vhcovr + covdir + run + f, shell = True)
