b80208bcac296e4e7c5b384e0bb28c8033e0936c
00650614 <00650614@hpc01.fpg.local>
2022-03-16 10:40:12 +0100

    added prefix selection for input bam (snv, cnv, rna)

--- Lmodules/cvLaunch.py
+++ Lmodules/cvLaunch.py
@@ -20,19 +20,19 @@ COV_MODULE = os.path.join(LIANNE_FOLDER, 'Lmodules/coverage.py')
 
 
 
-def main(email, out_localApp, debug):
+def main(email, out_localApp, debug, ot):
 
 	par = '#! /bin/bash\n\
 \n\
 #PBS -o /data/novaseq_results/'+out_localApp+'/stdout_coverage\n\
 #PBS -e /data/novaseq_results/'+out_localApp+'/stderr_coverage\n\
 #PBS -l select=1:ncpus=2:mem=5g\n'
-if email == 'noEmail':
-	pass 
-else:
-	par = par+"#PBS -M "+email+"\n"
-	par = par+"#PBS -m ae\n"
-par = par+"#PBS -N lianne_coverage\n\
+	if email == 'noEmail':
+		pass 
+	else:
+		par = par+"#PBS -M "+email+"\n"
+		par = par+"#PBS -m ae\n"
+	par = par+"#PBS -N lianne_coverage\n\
 #PBS -q workq\n\n"
 
 
@@ -52,9 +52,18 @@ par = par+"#PBS -N lianne_coverage\n\
 	coverage_file_run = os.path.join(out_localApp, 'coverage_run.sh')
 
 	# retrieve bam file path for coverage analysis
-	snv_bamDir = os.path.join(out_localApp, 'Logs_Intermediates/StitchedRealigned')
+	if ot == 'snv':
+		bamDir = os.path.join(out_localApp, 'Logs_Intermediates/StitchedRealigned')
+	elif ot == 'cnv':
+		bamDir = os.path.join(out_localApp, 'Logs_Intermediates/DnaRealignment')
+	elif ot == 'rna':
+		bamDir = os.path.join(out_localApp, 'Logs_Intermediates/RnaAlignment')
+	else:
+		print('[WARNING] bam file type not recognized for coverage')
+		print('[WARNING] exit')
+		os.sys.exit()
 	bam_list = []
-	for root, dirs, file in os.walk(snv_bamDir):
+	for root, dirs, file in os.walk(bamDir):
 		for f in file:
 			if f.endswith('.bam'):
 				bam_file = os.path.join(root, f)
@@ -80,6 +89,8 @@ par = par+"#PBS -N lianne_coverage\n\
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Launch script for coverage - Lianne module')
 
+##################################################################################################
+
 	# arguments
 	parser.add_argument('-e', '--email', required=False,
 						help='sh parameters built by Lianne',
@@ -89,11 +100,17 @@ if __name__ == '__main__':
 	parser.add_argument('-d', '--debug', required=False,
 						action='store_true',
 						help='Run the script in debug mode\nNo jobs will be send\nNo file will be written - Default=False')
+	parser.add_argument('-o', '--output_prefix', required=True,
+						help="Prefix of output folder.\nSelections allowed: snv, rna, cnv",
+						choices=['snv', 'rna', 'cnv'])
+
+##################################################################################################
 
 	args = parser.parse_args()
 	email = args.email
 	out_localApp = args.outLocalApp
 	debug = args.debug
+	ot = args.output_prefix
 
-	main(email, out_localApp, debug)
+	main(email, out_localApp, debug, ot)
 
